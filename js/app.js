f80e92883128c83c81f76d3cef53f8fbbde8317c
Hiroki KIYOHARA <hirokiky@gmail.com>
2015-04-21 16:40:27 +0900

    Made it to handle multiple memos

--- js/app.js
+++ js/app.js
@@ -1,24 +1,52 @@
 ko.bindingHandlers['codeMirror'] = {
   init: function(element, valueAccessor, allBindings) {
-    this.mainCode = CodeMirror.fromTextArea(
+    var value = valueAccessor();
+    var mainCode = CodeMirror.fromTextArea(
       element,
       {
         mode: "markdown",
         lineNumbers: true
       }
     );
-    this.mainCode.on('change', function (cm) {
-      var value = valueAccessor();
+    mainCode.on('change', function (cm) {
       value(cm.getValue());
     });
+
+    value.mainCode = mainCode;
+
     ko.bindingHandlers['value'].init(element, valueAccessor, allBindings);
   },
   update: ko.bindingHandlers['value'].update
 };
 
+
+var Memo = function() {
+  var self = this;
+
+  self.text = ko.observable("default");
+  self.updatedAt = new Date();
+  self.createdAt = new Date();
+
+  self.title = ko.computed(function() {
+    // FIXME: Take title from CodeMirror.title
+    return self.text().split('\n')[0]
+  });
+};
+
+
 var OtterViewModel = function() {
   var self = this;
-  self.code = ko.observable().extend({persist: 'otterCode'});
+  self.memos = ko.observableArray().extend({persist: 'otterMemos'});
+  self.numMemos = ko.computed(function() {
+    return self.memos().length
+  });
+  self.chosenMemoIdx = ko.observable();
+  self.chosenMemo = ko.computed(function() {
+    return self.memos()[self.chosenMemoIdx()]
+  });
+
+  self.memos([new Memo()]);
+  self.chosenMemoIdx(0);
 };
 
 window.addEventListener("load", function() {
